1807dfb7c9ef70b9ab6425f25f91d1da58db4fc9
Michael Shick <m@shick.us>
2020-08-04 16:58:12 -0400

    ci: fix labels path

--- labels/sync.sh
+++ labels/sync.sh
@@ -4,12 +4,13 @@ __dirname="${0%/*}"
 
 access_token=${1:-$PERSONAL_ACCESS_TOKEN}
 repos="${__dirname}/repos"
+labels="${__dirname}/labels.yml"
 
-if [ -z "$access_token" ]; then
+if [ -z "${access_token}" ]; then
   echo "personal access token is required"
   exit 1
 fi
 
 while IFS= read -r repo; do
-  npx github-label-sync --allow-added-labels --labels labels.yml --access-token $access_token $repo
-done < "$repos"
+  npx github-label-sync --allow-added-labels --labels ${labels} --access-token ${access_token} ${repo}
+done < "${repos}"
