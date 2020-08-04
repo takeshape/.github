1f742f312fabb393997a3316ef128e1995baff49
Michael Shick <m@shick.us>
2020-08-04 15:16:17 -0400

    label sync

---
+++ sync-labels.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+access_token=${1:-$PERSONAL_ACCESS_TOKEN}
+repos="./label-repos"
+
+if [ -z "$access_token" ]; then
+  echo "personal access token is required"
+  exit 1
+fi
+
+while IFS= read -r repo; do
+  npx github-label-sync --allow-added-labels --labels labels.yml --access-token $access_token $repo 
+done < "$repos"
